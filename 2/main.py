de69b2420e6167e833de5805db98fa605a4e89bb
veljkoprodan <57093756+veljkoprodan@users.noreply.github.com>
2022-11-10 20:48:17 +0100

    fajlovi

--- 2/main.py
+++ 2/main.py
@@ -1,15 +1,17 @@
 import matplotlib.pyplot as pyplot
 import naivni
 import dlt
+import dlt_norm
+import promena_koord
 
 def draw(ulazne, slike):
-    xs = [p[0] for p in ulazne]
-    ys = [p[1] for p in ulazne]
+    xs = [t[0] for t in ulazne]
+    ys = [t[1] for t in ulazne]
     xs.append(ulazne[0][0])
     ys.append(ulazne[0][1])
 
-    xd = [p[0] for p in slike]
-    yd = [p[1] for p in slike]
+    xd = [t[0] for t in slike]
+    yd = [t[1] for t in slike]
     xd.append(slike[0][0])
     yd.append(slike[0][1])
 
@@ -28,15 +30,92 @@ slike = [[-2, -1, 1],
          [2, 1, 1],
          [-2, 1, 1]]
 
-draw(ulazne, slike)
+# draw(ulazne, slike)
 
-#naivni
-M = naivni.alg(ulazne, slike)
-print(M.round(5))
+#Naivni
+print('\n----------------------------------------------')
+print('Resenje sa naivnim algoritmom:\n')
+M_naivni = naivni.alg(ulazne, slike)
+print(M_naivni.round(5))
+print('\n----------------------------------------------')
 
-#dlt
-M = dlt.alg(ulazne, slike)
-print(M.round(5))
+#DLT
+print('\nResenje sa DLT algoritmom:\n')
+M_dlt = dlt.alg(ulazne, slike)
+print(M_dlt.round(5))
+print('\n----------------------------------------------')
+
+#Poredjenje DLT i naivnog
+print('\nPoredjenje naivnog i DLT:\n')
+M_dlt = (M_dlt / M_dlt[0, 0]) * M_naivni[0, 0]
+print('Naivni:')
+print(M_naivni.round(5))
+print('\nDLT:')
+print(M_dlt.round(5))
+print('\n----------------------------------------------')
+
+
+#DLT sa normalizacijom
+print('Resenje sa modifikovanim DLT algoritmom:\n')
+M_dlt_norm = dlt_norm.alg(ulazne, slike)
+print(M_dlt_norm.round(5))
+print('\n----------------------------------------------')
+
+#Poredjenje DLT i DLT sa normalizacijom
+print('Poredjenje DLT i modifikovanog DLT:\n')
+M_dlt_norm = (M_dlt_norm / M_dlt_norm[0, 0]) * M_dlt[0, 0]
+print('DLT:')
+print(M_dlt.round(5))
+print('\nDLT sa normalizacijom:')
+print(M_dlt_norm.round(5))
+print('\n----------------------------------------------')
+
+#DLT sa vise korespodencija
+print('DLT sa vise korespodencija:\n')
+ulazne = [[-3, -1, 1],
+          [3, -1, 1],
+          [1, 1, 1],
+          [-1, 1, 1],
+          [1, 2, 3],
+          [-8, -2, 1]]
+
+slike = [[-2, -1, 1],
+         [2, -1, 1],
+         [2, 1, 1],
+         [-2, 1, 1],
+         [2, 1, 4],
+         [-16, -5, 4]]
+
+M_dlt2 = dlt.alg(ulazne, slike)
+print(M_dlt2.round(5))
+M_dlt2 = (M_dlt2 / M_dlt2[0, 0]) * M_naivni[0, 0]
+print('\n')
+print(M_dlt2.round(5))
+print('\nNaivni:')
+print(M_naivni.round(5))
+print('\nDLT:')
+print(M_dlt2.round(5))
+print('\n----------------------------------------------')
+
+#Modifikovani DLT sa vise korespodencija
+print('Modifikovani DLT sa vise korespodencija:\n')
+M_dlt_norm2 = dlt_norm.alg(ulazne, slike) #todo
+print(M_dlt_norm2.round(5))
+M_dlt_norm2 = (M_dlt_norm2 / M_dlt_norm2[0, 0]) * M_naivni[0, 0]
+print('\n')
+print(M_dlt_norm2.round(5))
+print('\nNaivni:')
+print(M_naivni.round(5))
+print('\nDLT sa normalizacijom:')
+print(M_dlt_norm2.round(5))
+print('\n----------------------------------------------')
+
+#Promena koordinata kod DLT algoritma
+promena_koord.alg(ulazne, slike)
+
+
+
+
 
 
 
